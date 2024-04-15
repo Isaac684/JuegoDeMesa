ed8a4e65045fb6ed6659b3a702dbfedd2e8cf793
Isaac684 <hc21018@ues.edu.sv>
2024-04-14 22:46:08 -0600

    Combinacion de colores a diversos textos

--- jugador.py
+++ jugador.py
@@ -1,29 +1,31 @@
 import random as rdn
 from juego import juego
-
+import colorama
+from colorama import Fore, Back, Style
 class jugador:
-    def __init__(self, id, posicionActual, resultadoDado,tiroDoble,cantidadTirosDobles,penalizacion,salidaMeta):
+    def __init__(self, id, posicionActual, resultadoDado,tiroDoble,cantidadTirosDobles,penalizacion,salidaInicio):
         self.id = id #entero para identificar jugador
         self.posicionActual = posicionActual #entero que indica la casilla actual en la que se encuentra el jugador
         self.resultadoDado = resultadoDado #arreglo que contiene los dos valores de los dados lanzados
         self.tiroDoble = tiroDoble #boleano que indica si el jugador va a tirar por segunda vez
         self.cantidadTirosDobles = cantidadTirosDobles #entero que indica la cantidad de veces dobles que ha tirado el jugador
         self.penalizacion = penalizacion #boooleano que indica si un jugador fue penalizado
-        self.salidaMeta = salidaMeta #booleano que indica si el jugado ha sacado par y este sale a partida
+        self.salidaInicio = salidaInicio #booleano que indica si el jugado ha sacado par y este sale de inicio
 
     #Funcion que sirve para saber si al lanzar los dados cayo en numero par
     def esPar(self,Boolpartida):
         if self.resultadoDado[0] == self.resultadoDado[1]:
             if Boolpartida: 
-                print("Haz obtenido dobles!!!!\nPuedes volver a tirar.")
                 self.tiroDoble = True
                 self.cantidadTirosDobles += 1
+                if(self.cantidadTirosDobles<=2):    
+                    print(f"{Back.GREEN}Haz obtenido dobles!!!!{Back.RESET}\nPuedes volver a tirar.")
             else:
-                self.salidaMeta = True
-                print(f"Jugador {self.id} ha salido de la meta porque haz sacado par")
+                self.salidaInicio = True
+                print(Fore.GREEN + f"Jugador {self.id} ha salido de la meta porque haz sacado par"+Fore.RESET)
         elif not Boolpartida:
             
-            print(f"No haz sacado par, por lo tanto no saldras de la meta.")
+            print(Fore.RED+f"No haz sacado par, por lo tanto no saldras de la meta."+Fore.RESET)
 
         else:
             self.tiroDoble = False
@@ -37,24 +39,24 @@ class jugador:
                 if self.posicionActual > 100:
                     extra = self.posicionActual - 100
                     self.posicionActual = self.posicionActual - extra
-                print(f"Te mueves a la casilla: {self.posicionActual}")
+                print(f"{Style.BRIGHT} Te mueves a la casilla: {self.posicionActual}{Style.RESET_ALL}")
                 if self.posicionActual in juego.casillasSeguras:
-                    print('Felicidades haz caido en una casilla segura!!!\nSignifica que no podran comerte si otro jugador cae en esta casilla :)')
+                    print(f'{Back.BLUE}Felicidades haz caido en una casilla segura!!!{Back.RESET}\nSignifica que no podran comerte si otro jugador cae en esta casilla.')
                 elif self.posicionActual in juego.tunelSeguro:
-                    print('Felicidades haz llegado al tunel seguro!!!\nApartir de ahora NADIE podra comerte.')
+                    print(f'{Back.BLUE}Felicidades haz llegado al tunel seguro!!!{Back.RESET}\nApartir de ahora NADIE podra comerte.')
 
     def lanzamientoInicial(self):
         print("\nLanzando dados...")
         self.resultadoDado[0] = rdn.randrange(1,6)
         self.resultadoDado[1] = rdn.randrange(1,6)
-        print(f"Dado 1: {self.resultadoDado[0]} Dado 2: {self.resultadoDado[1]}")
-        self.esPar(self.salidaMeta)
+        print(f"{Style.BRIGHT}Resultado de los dados: {self.resultadoDado[0]} y {self.resultadoDado[1]}{Style.RESET_ALL}")
+        self.esPar(self.salidaInicio)
 
     def lanzamientoDeBonificacion(self):
         print("\nLanzando dados...")
         self.resultadoDado[0] = rdn.randrange(1,6)
         self.resultadoDado[1] = rdn.randrange(1,6)
-        print(f"Dado 1: {self.resultadoDado[0]} Dado 2: {self.resultadoDado[1]}")
+        print(f"{Style.BRIGHT}Resultado de los dados: {self.resultadoDado[0]} y {self.resultadoDado[1]}{Style.RESET_ALL}")
         self.cambiarPosicion()
         if self.tiroDoble:
-            print("Volveras a tirar ya que habias dobles antes de caer en la casilla")
+            print(f"{Fore.GREEN}Volveras a tirar ya que habias dobles antes de caer en la casilla{Fore.RESET}")
--- tablero.py
+++ tablero.py
@@ -1,56 +1,58 @@
 import colorama
-from colorama import Fore, Back, Style
+from colorama import Fore, Back, Style  # Importa los estilos de color de la librería Colorama
 
-#tabla con formato
+# Define la clase Tablero
 class tablero:
-    def mostrarTablero(self, listJugadores,reglas): 
-        iconoJugador = ['  ⓵', '  ⓶', '  ⓷', '  ⓸', '  ⓹']
-        posicionesJugadores = []
+    # Método para mostrar el tablero con formato
+    def mostrarTablero(self, listJugadores, reglas): 
+        iconoJugador = ['  ⓵', '  ⓶', '  ⓷', '  ⓸', '  ⓹']  # Iconos para representar a los jugadores en el tablero
+        posicionesJugadores = []  # Lista para almacenar las posiciones actuales de los jugadores
+
+        # Obtiene las posiciones actuales de los jugadores
         for k in range(len(listJugadores)):
             posicionesJugadores.append(listJugadores[k].posicionActual)
 
-        tabla = []
+        tabla = []  # Lista para almacenar las filas del tablero
 
+        # Construye la tabla del tablero
         for i in range(1, 101, 10):
-            fila = []
+            fila = []  # Lista para almacenar una fila del tablero
             for j in range(i, i+10):
+                # Verifica en qué tipo de casilla se encuentra y asigna el formato correspondiente
                 if j in reglas.casillasPenalizacion:
-                    
                     if j in posicionesJugadores:
-                        fila.append(Fore.YELLOW + Back.RED +"{:4}".format(iconoJugador[posicionesJugadores.index(j)]) + " " + Back.RESET + Fore.RESET)  
+                        fila.append(Fore.YELLOW + Back.RED + "{:4}".format(iconoJugador[posicionesJugadores.index(j)]) + " " + Back.RESET + Fore.RESET)  
                     else:
-                        fila.append(Fore.YELLOW + Back.RED +"{:4}".format(j) + " " + Back.RESET + Fore.RESET)  
+                        fila.append(Fore.YELLOW + Back.RED + "{:4}".format(j) + " " + Back.RESET + Fore.RESET)  
 
                 elif j in reglas.casillasSeguras or j in reglas.tunelSeguro:
                     if j in posicionesJugadores:
-                        fila.append(Back.CYAN + "{:4}".format(iconoJugador[posicionesJugadores.index(j)]) +" "+ Back.RESET+Fore.RESET)  # Si no es par, imprimir normalmente
+                        fila.append(Back.CYAN + "{:4}".format(iconoJugador[posicionesJugadores.index(j)]) + " " + Back.RESET + Fore.RESET)
                     else:
-                        fila.append(Back.CYAN + "{:4}".format(j) +" "+ Back.RESET+Fore.RESET)  # Si no es par, imprimir normalmente
+                        fila.append(Back.CYAN + "{:4}".format(j) + " " + Back.RESET + Fore.RESET)
 
                 elif j in reglas.casillasTirosDoble:
                     if j in posicionesJugadores:
-                        fila.append(Fore.WHITE + Back.GREEN +"{:4}".format(iconoJugador[posicionesJugadores.index(j)]) +" "+ Back.RESET + Fore.RESET)
+                        fila.append(Fore.WHITE + Back.GREEN + "{:4}".format(iconoJugador[posicionesJugadores.index(j)]) + " " + Back.RESET + Fore.RESET)
                     else:
-                        fila.append(Fore.WHITE + Back.GREEN +"{:4}".format(j) +" "+ Back.RESET + Fore.RESET)
+                        fila.append(Fore.WHITE + Back.GREEN + "{:4}".format(j) + " " + Back.RESET + Fore.RESET)
 
                 else:
                     if j in posicionesJugadores:
                         fila.append("{:4}".format(iconoJugador[posicionesJugadores.index(j)]) + " ")  
                     elif j == 100:
-                        fila.append(Fore.WHITE + Back.LIGHTBLACK_EX +"{:4}".format('  Ⓜ') +" "+ Back.RESET + Fore.RESET)
+                        fila.append(Fore.WHITE + Back.LIGHTBLACK_EX + "{:4}".format('  Ⓜ') + " " + Back.RESET + Fore.RESET)
                     else:
                         fila.append("{:4}".format(j) + " ")  
 
-            tabla.append(fila)
+            tabla.append(fila)  # Agrega la fila a la tabla
 
-        # Imprimir la lista como tabla con el formato deseado
+        # Imprime el encabezado del tablero
         print('+-----------------------------+')
         print('|            INICIO           |')
         print('|', end='')
-        # Asegúrate de que el número ingresado por el usuario no supere la longitud del arreglo iconoJugador
-        #jugadores = min(jugadores, len(iconoJugador))
 
-        # Itera sobre los jugadores e imprime los iconos correspondientes
+        # Itera sobre los jugadores e imprime los iconos correspondientes en el inicio
         for i in range(len(listJugadores)):
             if listJugadores[i].posicionActual > 0:
                 continue
@@ -59,11 +61,10 @@ class tablero:
             
         print('\n+-----------------------------+\n')
 
-
+        # Imprime el cuerpo del tablero
         print('+-----------------------------------------------------------+')
         for fila in tabla:
-            
-            print('|' + '|'.join(fila)+"|")
+            print('|' + '|'.join(fila) + "|")
             print('+-----------------------------------------------------------+')
 
-        print()
+        print()  # Imprime una línea en blanco al final
